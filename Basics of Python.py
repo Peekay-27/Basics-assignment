32bab663f79cb808ef3bcbee2fa3f229d6916e6b
Peekay-27 <kwamekwarteng266@gmail.com>
2025-12-10 16:39:09 +0000

    Create Basics of Python.py

---
+++ Basics of Python.py
@@ -0,0 +1,13 @@
+Playlist = ["Unlimited","Imagine","Hotel California", "Billie Jean", "Smells Like Teen Spirit"
+]
+top_artists = ( "Beyoncé", "Cynthia Erivo", "Taylor Swift","Kendrick Lamar"
+)
+my_music= [["Unlimited","Imagine","Hotel California", "Billie Jean", "Smells Like Teen Spirit"
+],( "Beyoncé", "Cynthia Erivo", "Taylor Swift","Kendrick Lamar"
+)]
+print(len(my_music))
+print(Playlist[2])
+Playlist[0] = "Perfect"
+Playlist.append("Blinding Lights")
+Playlist=["Perfect","Imagine","Hotel California", "Billie Jean", "Smells Like Teen Spirit" , "Blinding lights"
+]
